5165238a4412319c4197feb1d72b5af1b4837bba
Mayur Pande <mayurpande.uk@gmail.com>
2016-04-28 12:46:56 +0100

    Register twig-view component as a service on the slim's app container

--- index.php
+++ index.php
@@ -6,12 +6,26 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require __DIR__ . '/vendor/autoload.php';
 date_default_timezone_set ( "Europe/London" );
 
-$app = new \Slim\App;
+$app = new \Slim\App();
+
+// Get container
+$container = $app->getContainer();
+
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('/templates', [
+		'debug' => true,
+    ]);
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container['router'],
+        $container['request']->getUri()
+    ));
+
+    return $view;
+};
 
 $app->get('/', function(Request $request, Response $response) {
-	$response = $this->view->render($response, "home.html");
-
-	return $response;
+	return  $this->view->render($response, "home.html");
 });
 
 $app->run();
