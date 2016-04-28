992de1e6dd7ed42faaffd1c55cdd3dec97c7df20
Mayur Pande <mayurpande.uk@gmail.com>
2016-04-28 13:39:49 +0100

    moved boostrapping code to seperate file and folder

--- index.php
+++ index.php
@@ -1,32 +1,7 @@
 <?php
-
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
-
-require __DIR__ . '/vendor/autoload.php';
-date_default_timezone_set ( "Europe/London" );
-
-$app = new \Slim\App();
-
-// Get container
-$container = $app->getContainer();
-
-// Register component on container
-$container['view'] = function ($container) {
-	$view = new \Slim\Views\Twig(__DIR__ . '/templates', [
-		'debug' => true,
-		'cache' => false,
-    ]);
-    $view->addExtension(new \Slim\Views\TwigExtension(
-        $container['router'],
-        $container['request']->getUri()
-    ));
-
-    return $view;
-};
-
+require __DIR__ . ('/bootstrap/app.php');
 $app->get('/', function(Request $request, Response $response) {
-	return  $this->view->render($response, "home.html");
+	return  $this->view->render($response, "home.twig");
 });
 
 $app->run();
