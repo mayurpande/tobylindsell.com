b19d2910363fbcfc5c12a82b1d9e48ae49c6eacf
Mayur Pande <mayurpande.uk@gmail.com>
2016-04-28 15:45:21 +0100

    Tried to acces views with object method but not allowed

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -5,7 +5,10 @@ namespace App\Controllers;
 class HomeController{
 
 	public function index($request, $response){
-		return 'Home controller';
+	/* we cannot access the views with this line below
+	 *
+	 * return $this->view->render($response, 'home.twig')
+	 */
 	}
 }
 
