e76c423fff391bb05c1725bf59b1c51f4f31e017
Mayur Pande <mayurpande.uk@gmail.com>
2016-04-28 13:48:18 +0100

    modified index file according to bootstrap files and route files

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
 require __DIR__ . ('/bootstrap/app.php');
-$app->get('/', function(Request $request, Response $response) {
-	return  $this->view->render($response, "home.twig");
-});
 
 $app->run();
