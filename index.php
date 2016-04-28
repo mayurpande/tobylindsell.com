340c9b7b2b6d62ad567b4daf299157a729d65c46
Mayur Pande <mayurpande.uk@gmail.com>
2016-04-28 12:33:06 +0100

    Moved html code to seperate file, started building slim controller

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
+<?php
 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-<html>
-	<head>
-		<title>Welcome to Toby.com!</title>
-		<link rel="stylesheet" type="text/css" href="css/normalize.css" />
-		<link rel="stylesheet" type="text/css" href="css/style.css" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	</head>
-	<body>
-		<div id="page-background">
-			<img src="img/main-background.jpg" width="100%" alt="Background Image" />
-		</div>
-	</body>
-</html>
+require __DIR__ . '/vendor/autoload.php';
+date_default_timezone_set ( "Europe/London" );
 
+$app = new \Slim\App;
 
+$app->get('/', function(Request $request, Response $response) {
+	$response = $this->view->render($response, "home.html");
+
+	return $response;
+});
+
+$app->run();
