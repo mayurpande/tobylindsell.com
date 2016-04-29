0832123b97a6b7b35a8b97c9bb5f590b993b7d6f
Mayur Pande <mayurpande.uk@gmail.com>
2016-04-29 15:31:59 +0100

    Run query on model and obtained data from home_page table and passed in key using assocative array

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controllers;
 
+//import model name
+use App\Models\Home_Page;
+
 //necessary because we are passing slim views instance
 use Slim\Views\Twig as View;
 
@@ -11,7 +14,8 @@ class HomeController extends Controller{
 	public function index($request, $response){
 	 //now we can use this view object and render the views
 	//we now have access to our whole container because we have the container in our base controller 
-		return $this->view->render($response, 'home.twig');
+		$home_page = Home_Page::where('id',1)->first();
+		return $this->view->render($response, 'home.twig', array('homePage' => $home_page));
 	}
 }
 
