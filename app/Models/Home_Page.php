d80406bc15f53bf494d2fef0f373d120c7eb7e01
Mayur Pande <mayurpande.uk@gmail.com>
2016-04-29 14:59:45 +0100

    Created model for homepage to use with db table

---
+++ app/Models/Home_Page.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+//we need to extend models eloquent's class
+use Illuminate\Database\Eloquent\Model;
+
+
+
+class Home_Page extends Model{
+//by doing this we now have the ability to use this class as a direct connection to our db table
+
+	//eloquent takes the singular version of the class name and it will automatically look for a table
+	//with the plural version. If your table differs we can explicitly name it like below
+	protected $table = 'home_page';
+
+
+}
