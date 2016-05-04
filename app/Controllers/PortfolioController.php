3fb1fbdb411774d4fbc68d68ca8c57bb8cce0612
Mayur Pande <mayurpande.uk@gmail.com>
2016-05-04 18:55:45 +0100

    Called model from fn to use with template file

--- app/Controllers/PortfolioController.php
+++ app/Controllers/PortfolioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Models\Port_Page
 //necessary because we are passing slim views instance
 use Slim\Views\Twig as View;
 
@@ -10,7 +11,8 @@ class PortfolioController extends Controller{
 
 	public function index($request, $response){
 	 //now we can use this view object and render the views
-	//we now have access to our whole container because we have the container in our base controller 
-		return $this->view->render($response, 'portfolio.twig');
+		//we now have access to our whole container because we have the container in our base controller
+		$portPage = Port_Page::where('id',1)->first(); 
+		return $this->view->render($response, 'portfolio.twig',array('portPage' => $portPage);
 	}
 }
