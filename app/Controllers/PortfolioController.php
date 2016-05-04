044c0f84af9fade8a266a3ea982509d55d6ae5a9
Mayur Pande <mayurpande.uk@gmail.com>
2016-05-04 19:29:34 +0100

    Obtained data from model and passed in array to view

--- app/Controllers/PortfolioController.php
+++ app/Controllers/PortfolioController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controllers;
 
-use App\Models\Port_Page
+use App\Models\Port_Page;
 //necessary because we are passing slim views instance
 use Slim\Views\Twig as View;
 
@@ -12,7 +12,10 @@ class PortfolioController extends Controller{
 	public function index($request, $response){
 	 //now we can use this view object and render the views
 		//we now have access to our whole container because we have the container in our base controller
-		$portPage = Port_Page::where('id',1)->first(); 
-		return $this->view->render($response, 'portfolio.twig',array('portPage' => $portPage);
+		$portPage = Port_Page::all();
+		foreach($portPage as $id){
+			$item = $id->id;
+		}
+		return $this->view->render($response, 'portfolio.twig',['portPage' => $portPage,'item' => $item]);
 	}
 }
