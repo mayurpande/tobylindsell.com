21bdfee9cb8197fa55ef585d8b35c40e801d7145
Mayur Pande <mayurpande.uk@gmail.com>
2016-05-12 15:49:50 +0100

    Added route for admin login

--- app/routes.php
+++ app/routes.php
@@ -9,3 +9,7 @@ $app->get('/what-we-do','WhatWeDoController:index')->setName('what');
 $app->get('/about-us','AboutUsController:index')->setName('about');
 
 $app->get('/contact-us','ContactUsController:index')->setName('contact');
+
+$app->get('/admin/login','AuthController:getSignIn')->setName('auth.signin');
+
+
