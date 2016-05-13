05e0a1a49eedda1c75b18eea9f49358ab3ae9e68
Mayur Pande <mayurpande.uk@gmail.com>
2016-05-13 12:38:13 +0100

    Changed functions so that they are sign up fns instead

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers\Auth;
 
+use App\Models\Admin;
+
 use App\Controllers\Controller;
 
 //import validator
@@ -10,14 +12,14 @@ use Respect\Validation\Validator as v;
 class AuthController extends Controller{
 
 	//fn to render login page for admin
-	public function getSignIn($request,$response){
+	public function getSignUp($request,$response){
 		
-		return $this->view->render($response, 'auth/admin-login.twig');
+		return $this->view->render($response, 'auth/admin-signup.twig');
 	
 	}
 
 	//fn for post login, this will be passed over to the validate method in the validator class
-	public function postSignIn(){
+	public function postSignUp($request,$response){
 
 		//here we use the validation object. here we also have an array of rules
 		//in this case email and password need to be available, 
@@ -25,16 +27,23 @@ class AuthController extends Controller{
 			//for the data provided, for this form input, we want no white space and we don't want
 			//this to be empty, 
 			'email' => v::noWhitespace()->notEmpty(),
-			'name' => v::notEmpty()->alpha();
+			'name' => v::notEmpty()->alpha(),
 			'password' => v::noWhitespace()->notEmpty(),
 
 		]);		
+
+		if($validation->failed()){
+			//if the validation failed redirect to signup page
+			return $response->withRedirect($this->router->pathFor('auth.signup'));
+		}
 
 		$admin = Admin::create([
 			'email' => $request->getParam('email'),
 			'name' => $request->getParam('name'),
 			'password' => password_hash($request->getParam('password'),PASSWORD_DEFAULT),
-		]};
+		]);
+
+		return $response->withRedirect($this->router->pathFor('home'));
 	
 	}	
 }
