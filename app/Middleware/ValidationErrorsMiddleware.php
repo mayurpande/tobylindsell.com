0263fff47987915db702baa4b844db5fd2f4aa2d
Mayur Pande <mayurpande.uk@gmail.com>
2016-05-20 10:37:00 +0100

    Used session_start() method to invoke the session variable

--- app/Middleware/ValidationErrorsMiddleware.php
+++ app/Middleware/ValidationErrorsMiddleware.php
@@ -2,6 +2,8 @@
 
 namespace App\Middleware;
 
+session_start();
+
 //inherits base middleware
 class ValidationErrorsMiddleware extends Middleware{
 
@@ -9,24 +11,27 @@ class ValidationErrorsMiddleware extends Middleware{
 	//we pass into here our $request and $response object. And we also have the $next callable middleware
 	//it is important that we call the next middleware because it needs to cycle through from the outside
 	//of our application much like an onion into the core.
-	public function __invoke($request,$response,$next){
-		
+
+	public function __invoke($request,$response,$next){	
 		//before state has changed
 		/* over here we take errors from session validator
 		 * and then place errors into our views
 		 *
 		 * addGlobal takes two parameter, the first is the name the second is the actual global var in 
 		 * this case its the $_SESSION['errors']
-		 */		
-		$this->container->view->getEnvironment()->addGlobal('errors',$_SESSION['errors']);	
+		 */
+		$this->container->view->getEnvironment()->addGlobal('errors', $_SESSION['errors']);
+			
 		//we unset the session as we no longer need it
 		unset($_SESSION['errors']);
-
+		
 		//after state has changed
 		$response = $next($request,$response);
-
+	
 		return $response;
-	}
 	
 
-}
+	}
+}	
+
+
