cdd524af257cc95054f304593adbc599ea42a513
Mayur Pande <mayurpande.uk@gmail.com>
2016-06-09 17:48:37 +0100

    Simple change to syntax

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller{
 			return $response->withRedirect($this->router->pathFor('auth.signin'));
 		}
 
-		return $this->view->render($response, 'auth/admin.twig');
+		return $this->view->render($response, 'admin.twig');
 
 	}
 
