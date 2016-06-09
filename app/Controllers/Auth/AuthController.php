f993986b0f44f4b5c24902dccfe3dd3f1b9382d8
Mayur Pande <mayurpande.uk@gmail.com>
2016-06-09 16:29:09 +0100

    Added validation to getSignIn method

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -15,7 +15,23 @@ class AuthController extends Controller{
 	}
 
 	public function postSignIn($request,$response){
-		//
+		//here we use the validation object. here we also have an array of rules
+		//in this case email and password need to be available, 
+		$validation = $this->validator->validate($request, [
+			//for the data provided, for this form input, we want no white space and we don't want
+			//this to be empty, 
+			'email' => v::noWhitespace()->notEmpty()->email(),
+			'password' => v::noWhitespace()->notEmpty(),
+
+		]);		
+
+		if($validation->failed()){
+			//if the validation failed redirect to signup page
+			return $response->withRedirect($this->router->pathFor('auth.signin'));
+		}
+
+
+
 	}
 
 	//fn to render login page for admin
