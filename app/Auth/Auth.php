4fafa2fe2b14130a9a1360dee51c266f62ace952
Mayur Pande <mayurpande.uk@gmail.com>
2016-06-09 16:36:48 +0100

    tested for user and set session if true

--- app/Auth/Auth.php
+++ app/Auth/Auth.php
@@ -2,7 +2,31 @@
 
 namespace App\Auth;
 
+use App\Models\User;
+
 class Auth{
-	
+
+	public function attempt($email,$password){
+		
+		//grab user by email
+
+		$user = User::where('email',$email)->first();
+		
+		//if !user return false
+		if(!$user){
+			return false;
+		}
+
+		//verify password for that user
+		if(password_verify($password,$user->password)){
+			
+			//set user into session
+			$_SESSION['user'] = $user->id;
+
+			return true;
+		}
+
+		return false;
+	}	
 
 }
