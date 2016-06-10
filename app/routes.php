6459ffc1c4565c986b534be7c75c229f33591f1f
Mayur Pande <mayurpande.uk@gmail.com>
2016-06-10 16:27:52 +0100

    Created routes for what-we-do-info admin page

--- app/routes.php
+++ app/routes.php
@@ -37,3 +37,15 @@ $app->post('/admin-update-news','AdminController:postNewsUpdate');
 $app->get('/admin-update-what-info-1','AdminController:getWhat1Update')->setName('adminWhat1.update');
 
 $app->post('/admin-update-what-info-1','AdminController:postWhat1Update');
+
+$app->get('/admin-update-what-info-2','AdminController:getWhat2Update')->setName('adminWhat2.update');
+
+$app->post('/admin-update-what-info-2','AdminController:postWhat2Update');
+
+$app->get('/admin-update-what-info-3','AdminController:getWhat3Update')->setName('adminWhat3.update');
+
+$app->post('/admin-update-what-info-3','AdminController:postWhat3Update');
+
+$app->get('/admin-update-what-info-4','AdminController:getWhat4Update')->setName('adminWhat4.update');
+
+$app->post('/admin-update-what-info-4','AdminController:postWhat4Update');
