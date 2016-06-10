001a81a92c1f6d518ff2a63d9b3e226de33296f4
Mayur Pande <mayurpande.uk@gmail.com>
2016-06-10 11:51:21 +0100

    Create sign out function

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -10,6 +10,16 @@ use Respect\Validation\Validator as v;
 
 class AuthController extends Controller{
 	
+	
+	public function getSignOut($request,$response){
+
+		$this->auth->logout();
+
+		return $response->withRedirect($this->router->pathFor('home'));
+
+	
+	}
+
 	public function getSignIn($request,$response){
 		return $this->view->render($response,'auth/signin.twig');
 	}
@@ -27,7 +37,7 @@ class AuthController extends Controller{
 			return $response->withRedirect($this->router->pathFor('auth.signin'));
 		}
 
-		return $response->withRedirect($this->router->pathFor('admin.update'));
+		return $response->withRedirect($this->router->pathFor('home'));
 
 	}
 
