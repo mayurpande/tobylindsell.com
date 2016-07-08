9a91eb5fae1e247342ff476133489699615a6af3
Mayur Pande <mayurpande.uk@gmail.com>
2016-07-08 18:21:42 +0100

    Created controller for change password feature

---
+++ app/Controllers/Auth/PasswordController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers\Auth;
+
+use App\Models\Admin;
+use App\Controllers\Controller;
+
+//import validator
+use Respect\Validation\Validator as v;
+
+class PasswordController extends Controller
+{
+	public function getChangePassword($request,$response)
+	{
+
+
+	}
+
+
+	public function postChangePassword($request,$response)
+	{
+		
+	}
+}	
+	
+
