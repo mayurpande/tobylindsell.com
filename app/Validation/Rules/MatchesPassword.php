3f0a49a9edf7cd790d5e088bd4efadceadaa5b55
Mayur Pande <mayurpande.uk@gmail.com>
2016-08-04 12:45:17 +0100

    Added rule file for matching password

---
+++ app/Validation/Rules/MatchesPassword.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Validation\Rules;
+
+use App\Models\Admin;
+use Respect\Validation\Rules\AbstractRule;
+
+class MatchesPassword extends AbstractRule
+{
+    public function validate($input)
+    {
+        
+    }
+}
