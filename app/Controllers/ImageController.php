434c383255788066eba23dfb703c65e90998151d
Mayur Pande <mayurpande.uk@gmail.com>
2016-08-05 12:39:22 +0100

    Called codeguy/upload repo here to handle file upload

--- app/Controllers/ImageController.php
+++ app/Controllers/ImageController.php
@@ -3,30 +3,57 @@
 namespace App\Controllers;
 
 use App\Controllers\Controller;
+use Upload\Storage\FileSystem;
+use Upload\File;
+use Upload\Validation\MimeType;
+use Upload\Validation\Size;
 
 //import validator
 use Respect\Validation\Validator as v;
 
-class ImageController extends Controller{
+class ImageController extends Controller
+{
 
-
+    
 	public function getImageUpload($request,$response){
 		return $this->view->render($response,'admin-upload.twig');
 	}
 
-	public function postImageUpload($request,$response){
-        $files = $request->getUploadedFiles();
-        if (empty($files['upload'])) {
-            throw new Exception('Expected a new file');
-        }
+    public function postImageUpload($request,$response){
+        $dir = USER_ROOT;
+        $storage = new \Upload\Storage\FileSystem($_SERVER['DOCUMENT_ROOT'] . "/img");
+        $file = new \Upload\File('upload',$storage);
 
-        $newFile = $files['upload'];
+        $new_filename = uniqid();
+        $file->setName($new_filename);
 
-        if ($newfile->getError() === UPLOAD_ERR_OK) {
-            $uploadFileName = $newfile->getClientFilename();
-            $newfile->moveTo("/img/$uploadFileName");
-        }
+       
+        $file->addValidations(array(
+            new \Upload\Validation\Mimetype(array('image/png','image/gif','image/jpg','image/jpeg')),
 
+            new \Upload\Validation\Size('5M')
+        ));
+        
+
+        // Access data about the file that has been uploaded
+        $data = array(
+            'name'       => $file->getNameWithExtension(),
+            'extension'  => $file->getExtension(),
+            'mime'       => $file->getMimetype(),
+            'size'       => $file->getSize(),
+            'md5'        => $file->getMd5(),
+            'dimensions' => $file->getDimensions()
+        );
+        // Try to upload file
+        try {
+            // Success!
+            $file->upload();
+            return $response->withRedirect($this->router->pathFor('admin.update'));
+        } catch (\Exception $e) {
+            // Fail!
+            $errors = $file->getErrors();
+        }        
+       
 	
 	}
 
